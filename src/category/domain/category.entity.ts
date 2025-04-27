53bfb781748c854d4c75fb982f5ccfdb720382ee
Darlan Guimaraes <darlan.guimaraes@wihex.com.br>
2025-04-27 17:49:49 +0000

    first commit

---
+++ src/category/domain/category.entity.ts
@@ -0,0 +1,83 @@
+import { Entity } from "../../shared/domain/entity";
+import { EntityValidationError } from "../../shared/domain/validators/validation.error";
+import { ValueObject } from "../../shared/domain/value-object";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryValidatorFactory } from "./category.validator";
+
+export type CategoryConstructorProps = {
+  categoryId?: Uuid;
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+  createdAt?: Date;
+}
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+}
+
+export class Category extends Entity {
+  categoryId: Uuid;
+  name: string;
+  description: string | null;
+  isActive: boolean;
+  createdAt: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    super();
+    this.categoryId = props.categoryId ?? new Uuid();
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.isActive = props.isActive ?? true;
+    this.createdAt = props.createdAt ?? new Date();
+  }
+
+  get entityId(): ValueObject {
+    return this.categoryId;
+  }
+
+  static create(props: CategoryCreateCommand): Category {
+    const category = new Category(props);
+    Category.validate(category);
+    return category;
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+    Category.validate(this);
+
+  }
+
+  changeDescription(description: string): void {
+    this.description = description;
+    Category.validate(this);
+  }
+
+  activate() {
+    this.isActive = true;
+  }
+
+  deactivate() {
+    this.isActive = false;
+  }
+
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create();
+    const isValid = validator.validate(entity);
+    if (!isValid) {
+      throw new EntityValidationError(validator.errors);
+    }
+  }
+
+  toJSON() {
+    return {
+      categoryId: this.categoryId.id,
+      name: this.name,
+      description: this.description,
+      isActive: this.isActive,
+      createdAt: this.createdAt
+    }
+  }
+}
---
+++ src/category/domain/category.validator.ts
@@ -0,0 +1,34 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { Category } from './category.entity';
+import { ClassvalidatorFields } from '../../shared/domain/validators/class-validator-fields';
+
+export class CategoryRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string | null;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  constructor({ name, description, isActive }: Category) {
+    Object.assign(this, { name, description, isActive })
+  }
+}
+
+export class CategoryValidator extends ClassvalidatorFields<CategoryRules> {
+  validate(entity: Category) {
+    return super.validate(new CategoryRules(entity));
+  }
+}
+
+export class CategoryValidatorFactory {
+  static create() {
+    return new CategoryValidator();
+  }
+}
